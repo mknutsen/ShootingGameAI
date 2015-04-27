7a8f96534c40e9f431fe47a091d2d238f797d6c8
Max Knutsen <mknutsen99@gmail.com>
2015-04-27 12:34:52 -0400

    major update
    mostly moved graphics library to external project
    minor updates

--- src/aiproject/game/CompletionCallback.java
+++
@@ -1,7 +0,0 @@
-package aiproject.game;
-
-public abstract class CompletionCallback {
-
-	public abstract void execute(Object... returnValue);
-
-}
--- src/aiproject/player/ai/decisiontree/State.java
+++
@@ -1,5 +0,0 @@
-package aiproject.player.ai.decisiontree;
-
-public enum State {
-	STUNNED(), ATTACKING(), RUNNING(), SHOOTING(), SHIELDING();
-}
